f7733c2b2e65d48419710bb122e6da29b7c92f0f
Abacop6999 <abacopcal33333@gmail.com>
2023-03-30 09:27:26 -0500

    homeScreen

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@ const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
   const {
-<<<<<<< HEAD
     resolver: { sourceExts, assetExts},
   } = await getDefaultConfig();
   return {
@@ -18,16 +17,6 @@ module.exports = (async () => {
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
-=======
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
->>>>>>> develop
       sourceExts: [...sourceExts, "svg"],
     },
   };
