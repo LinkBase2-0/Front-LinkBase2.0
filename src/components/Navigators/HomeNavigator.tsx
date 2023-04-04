c3f365b6871059d3110d2ca88cafcab4ef98bc07
Fabio-Alessandro <fabioalessandrotr@gmail.com>
2023-04-04 11:29:40 -0600

    Fixed errors

--- src/components/Navigators/HomeNavigator.tsx
+++ src/components/Navigators/HomeNavigator.tsx
@@ -3,10 +3,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Home } from "../../screens/Usuario/Home/Home";
 import ProviderScreen from "../ProviderScreen";
+import CategoryDetail from "../../screens/Usuario/CategoryDetail/CategoryDetail";
 
 type RootStackParamList = {
   "Overview": undefined;
   "Provider": undefined; 
+  "Category Detail": { categoryName: string };
 }
 
 const HomeNavigator: React.FC = () => {
@@ -17,6 +19,7 @@ const HomeNavigator: React.FC = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Overview" component={Home}/>
       <Stack.Screen name="Provider" component={ProviderScreen}/>
+      <Stack.Screen name="Category Detail" component={CategoryDetail}/>
     </Stack.Navigator>
   );
 }
