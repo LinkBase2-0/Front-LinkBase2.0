a53e970022a8939434c5dab5fc70af815d53da8e
Abacop6999 <abacopcal33333@gmail.com>
2023-04-13 17:17:31 -0500

    carrousel-modified

--- src/screens/Usuario/Home/carousel/Carousel.tsx
+++ src/screens/Usuario/Home/carousel/Carousel.tsx
@@ -6,22 +6,22 @@ import {
   useWindowDimensions,
   Pressable,
 } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { 
-  Container, 
-  ScrollContainer, 
-  Card, 
-  IndicatorContainer, 
-  Title, 
-  StarContainer, 
-  StarText 
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import {
+  Container,
+  ScrollContainer,
+  Card,
+  IndicatorContainer,
+  Title,
+  StarContainer,
+  StarText,
 } from "./styles";
 import { Provider } from "../Home";
 import { StarSvg } from "../../../../assets/svgImages/Usuario/Home";
 
-type Props = { providers: Provider[] }
-type RootStackParamList = { Provider: { name: string } }
+type Props = { providers: Provider[] };
+type RootStackParamList = { Provider: { name: string } };
 
 const Carousel: React.FC<Props> = ({ providers }) => {
   const scrollX = useRef(new Animated.Value(0)).current;
@@ -32,17 +32,17 @@ const Carousel: React.FC<Props> = ({ providers }) => {
     const inputRange = [
       (index - 1) * windowWidth,
       index * windowWidth,
-      (index + 1) * windowWidth
+      (index + 1) * windowWidth,
     ];
     const dotWidth = scrollX.interpolate({
       inputRange,
       outputRange: [8, 9, 8],
-      extrapolate: "clamp"
+      extrapolate: "clamp",
     });
     const dotColor = scrollX.interpolate({
       inputRange,
       outputRange: ["#C4C4C4", "#828282", "#C4C4C4"],
-      extrapolate: "clamp"
+      extrapolate: "clamp",
     });
 
     return (
@@ -53,11 +53,13 @@ const Carousel: React.FC<Props> = ({ providers }) => {
           height: 8,
           borderRadius: 4,
           backgroundColor: dotColor,
-          marginHorizontal: 4
+          marginHorizontal: 4,
         }}
       />
     );
-  }
+  };
+
+  const providersToShow = providers.slice(0, 3);
 
   return (
     <Container>
@@ -66,37 +68,42 @@ const Carousel: React.FC<Props> = ({ providers }) => {
           horizontal={true}
           pagingEnabled
           showsHorizontalScrollIndicator={false}
-          onScroll={
-            Animated.event(
-              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-              { useNativeDriver: false }
-            )
-          }
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
           scrollEventThrottle={1}
-          contentContainerStyle={{ alignItems: "center", paddingVertical: 20 }}
+          contentContainerStyle={{
+            alignItems: "center",
+            paddingHorizontal: (windowWidth - 420) / 2, // 280 is the width of the card
+          }}
         >
-          {providers.map(provider => (
-            <Pressable 
+          {providersToShow.map((provider) => (
+            <Pressable
               key={provider.id}
               onPress={() => {
                 navigation.navigate("Provider", { name: provider.name });
-              }} 
+              }}
             >
               <View
                 key={provider.id}
-                style={{ width: windowWidth - 20, height: 200 }}
+                style={{
+                  width: windowWidth - 20,
+                  height: 200,
+                  marginHorizontal: 5,
+                  paddingRight: 15
+                }}
               >
                 <Card
                   key={provider.id}
-                  source={{ uri: provider.photoURL}}
+                  source={{ uri: provider.photoURL }}
                   style={{ elevation: 3 }}
                 ></Card>
                 <View style={{ paddingLeft: 15, marginTop: 5 }}>
                   <Title>{provider.name}</Title>
                   <StarContainer>
                     <StarSvg />
-                    <StarText>4.5 - 20 reseñas
-                    </StarText>
+                    <StarText>4.5 - 20 reseñas</StarText>
                   </StarContainer>
                 </View>
               </View>
@@ -104,11 +111,11 @@ const Carousel: React.FC<Props> = ({ providers }) => {
           ))}
         </ScrollView>
         <IndicatorContainer>
-          {providers.map((provider, index) => renderDots(index))}
+          {providersToShow.map((provider, index) => renderDots(index))}
         </IndicatorContainer>
       </ScrollContainer>
     </Container>
   );
-}
+};
 
 export default Carousel;
