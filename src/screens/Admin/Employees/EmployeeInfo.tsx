2554504e3d76c7c5998c89f397cc85e35cada28c
Abacop6999 <abacopcal33333@gmail.com>
2023-04-13 22:33:40 -0500

    employesScreen-and-integration

--- src/screens/Admin/Employees/EmployeeInfo.tsx
+++ src/screens/Admin/Employees/EmployeeInfo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Alert, TouchableOpacity, View } from "react-native";
 import {
   EmployeeContainer,
@@ -10,6 +10,9 @@ import {
 } from "./styles";
 import axios from "axios";
 import { ProfilePic } from "../../Usuario/Profile/styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwtDecode from "jwt-decode";
+
 
 interface Empleado {
   id: string;
@@ -21,14 +24,52 @@ interface Empleado {
   photoURL: string;
 }
 
-const EmployeeInfo: React.FC<Empleado> = ({
+interface EmployeeInfoProps extends Empleado {
+  onDelete: (id: string) => void;
+}
+
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  photoURL: string;
+  CompanyId: string;
+}
+
+const EmployeeInfo: React.FC<EmployeeInfoProps> = ({
   id,
   fullName,
   company,
   charge,
   email,
   photoURL,
+  onDelete,
 }) => {
+  console.log("IDUSER", id);
+
+  const [userInfo, setUserInfo] = useState<IUser | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const value = await AsyncStorage.getItem("token");
+      if (value !== null) {
+        setToken(value);
+        const decodedToken: any = jwtDecode(value);
+        const response = await axios.get(
+          `${process.env.IP_ADDRESS}/users/${decodedToken.user.id}`
+        );
+        const user = response.data as IUser;
+        setUserInfo(user);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("TOKEN", token);
+
   const handleDelete = async () => {
     const title = "Alerta";
     const message = "¿Eliminar usuario?";
@@ -37,7 +78,12 @@ const EmployeeInfo: React.FC<Empleado> = ({
         text: "Confirmar",
         onPress: async () => {
           try {
-            await axios.delete(`${process.env.IP_ADDRESS}/users/${id}`);
+            const config = {
+              headers: { Authorization: `Bearer ${token}` },
+              data: { token: token },
+            };
+            await axios.delete(`${process.env.IP_ADDRESS}/users/${id}`, config);
+            onDelete(id);
           } catch (error) {
             console.log(error);
           }
@@ -59,9 +105,11 @@ const EmployeeInfo: React.FC<Empleado> = ({
               ? photoURL
               : "https://assets.stickpng.com/thumbs/585e4beacb11b227491c3399.png",
           }}
-          style={{ height: 20, width: 20 , marginLeft: 15}}
+          style={{ height: 20, width: 20, marginLeft: 15 }}
         />
-        <FullName key={id} style={{marginLeft:12}}>{fullName}</FullName>
+        <FullName key={id} style={{ marginLeft: 12 }}>
+          {fullName}
+        </FullName>
       </View>
       <View
         style={{
